34ddc2be4bf7eb57d8ebde8ca5ebd3311a662d09
Aliou <badarak20@gmail.com>
2020-06-03 08:08:32 +0200

    - Adding route update person - updating mocks - updating unit test#

--- src/main/scala/Person.scala
+++ src/main/scala/Person.scala
@@ -1,3 +1,4 @@
 
 case class Person(id: String, firstName : String, lastName : String, age: Int)
 case class CreatePerson(firstName : String, lastName : String, age: Int)
+case class UpdatePerson(firstName : Option[String], lastName : Option[String], age: Option[Int])
--- src/main/scala/PersonRepository.scala
+++ src/main/scala/PersonRepository.scala
@@ -1,14 +1,19 @@
 import java.util.UUID
 
+import PersonRepository.PersonNotFound
+
 import scala.concurrent.{ExecutionContext, Future}
 
 trait PersonRepository {
-
   def all(): Future[Seq[Person]]
   def adults() : Future[Seq[Person]]
   def miners() : Future[Seq[Person]]
-
   def save(createPerson: CreatePerson): Future[Person]
+  def update(id : String, updatePerson: UpdatePerson) : Future[Person]
+}
+
+object PersonRepository {
+  final case class PersonNotFound(id : String) extends Exception(s"Person with id $id not found.")
 }
 
 class PersonRepositoryImpl(initialListOfPerson : Seq[Person] = Seq.empty)(implicit ec : ExecutionContext)
@@ -29,5 +34,24 @@ class PersonRepositoryImpl(initialListOfPerson : Seq[Person] = Seq.empty)(implic
 
     persons = persons :+ person
     person
+  }
+
+  override def update(id : String, updatePerson: UpdatePerson): Future[Person] = {
+    initialListOfPerson.find(_.id == id) match {
+      case Some(foundPerson) =>
+        val newPerson = updatePersonHelper(foundPerson, updatePerson)
+        persons = persons.map(p => if(p.id == id) newPerson else p)
+        Future.successful(newPerson)
+      case None =>
+        Future.failed(PersonNotFound(id))
+    }
+  }
+
+  private def updatePersonHelper(foundPerson: Person, updatePerson: UpdatePerson) : Person = {
+    val t1 = updatePerson.firstName.map(firstName => foundPerson.copy(firstName = firstName))
+                                   .getOrElse(foundPerson)
+    val t2 = updatePerson.lastName.map(lastName => t1.copy(lastName =lastName))
+                                  .getOrElse(t1)
+    updatePerson.age.map(age => t2.copy(age = age)).getOrElse(t2)
   }
 }
